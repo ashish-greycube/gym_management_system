5ac641cbdcab8d44551a2c329d106536b53aab13
puja-greycube <puja@greycube.in>
2023-07-03 15:00:05 +0530

    feat:group class web form

--- gym_management/gym_management/doctype/gym_group_classes/gym_group_classes.py
+++ gym_management/gym_management/doctype/gym_group_classes/gym_group_classes.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2023, puja and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe.website.website_generator import WebsiteGenerator
 from frappe.model.document import Document
 
-class GymGroupClasses(Document):
+class GymGroupClasses(WebsiteGenerator):
 	pass
--- gym_management/gym_management/doctype/gym_workout_plan/gym_workout_plan.py
+++ gym_management/gym_management/doctype/gym_workout_plan/gym_workout_plan.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2023, puja and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 from frappe.model.document import Document
 
-class GymWorkoutPlan(Document):
+class GymWorkoutPlan(WebsiteGenerator):
 	pass
